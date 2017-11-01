c4e888ca16a6ff79dc1f2fe1a459cf0299e79ec7
김호종 <herohjk@naver.com>
2017-11-01 16:27:24 +0900

    완성

--- 가계부/Program.cs
+++ 가계부/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,34 @@ namespace 가계부
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(ResolveAssembly);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly thisAssembly = Assembly.GetExecutingAssembly();
+            var name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+
+            var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
+            if (resources.Count() > 0)
+            {
+                string resourceName = resources.First();
+                using (Stream stream = thisAssembly.GetManifestResourceStream(resourceName))
+                {
+                    if (stream != null)
+                    {
+                        byte[] assembly = new byte[stream.Length];
+                        stream.Read(assembly, 0, assembly.Length);
+                        Console.WriteLine("Dll file load : " + resourceName);
+                        return Assembly.Load(assembly);
+                    }
+                }
+            }
+            return null;
         }
     }
 }
